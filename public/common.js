e208cb79ee2f580a23ed683f73c5e045c86c971e
arron_lyu <44466444+arronLyu1893@users.noreply.github.com>
2018-11-01 00:44:07 +0800

    Add files via upload

---
+++ public/common.js
@@ -0,0 +1,14 @@
+//获取页面元素
+function $(id){
+	return document.getElementById(id);
+}
+//创建异步对象
+function createXhr(){
+	var xhr=null;
+	if (window.XMLHttpRequest){
+		xhr=new XMLHttpRequest();
+	}else{
+		xhr=new ActiveXObject("Microsoft.XMLHttp");
+	}
+	return xhr;
+}
