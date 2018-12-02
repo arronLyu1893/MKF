8fbbd2548e6d38f2ec0e579ff375ca6f9940cf2f
arron_lyu <44466444+arronLyu1893@users.noreply.github.com>
2018-12-03 01:06:18 +0800

    Add files via upload

--- js/header.js
+++ js/header.js
@@ -7,6 +7,30 @@ $(function(){
 		type:"get",
 		success:function(res){
 			$("#header").replaceWith(res)
+			//1.查找触发事件的元素
+			var $btnSearch=$("#button"),
+			$input=$btnSearch.prev();
+
+			//2.绑定事件
+			$btnSearch.click(function(){
+				//3.查找要修改的元素
+				//4.修改元素
+				var kw=$input.val().trim();
+				if(kw!=="")
+					location.href=`products.html?kwords=${kw}`;
+			}) 
+			$input.keyup(function(e){
+				console.log(e.keyCode)
+				if(e.keyCode==13)
+					$btnSearch.click()
+			})
+			if(location.search.indexOf("kwords")!=-1){
+				var kwords=decodeURI(location.search.split("=")[1])
+				//kwords:  //?kwords=macbook			
+				$input.val(kwords)
+			}
+			
+			 
 		}
 	})
 })
---
+++ js/products.js
@@ -0,0 +1,79 @@
+$(function(){
+  if(location.search.indexOf("kwords=")!=-1){
+    var kwords=location.search.split("=")[1];
+    var pno=1;
+    $.ajax({
+      url:"http://localhost:3000/products",
+      type:"get",
+      data:{kwords,pno},
+      dataType:"json",
+      success:function(output){
+        console.log(output)
+        var {products}=output;
+        var html="";
+        for(var p of products){
+          var {lid,title,price,md} = p;
+          html+=`<li class="gl-item">
+              <div class="goods-content">
+                <div class="goods-pic">
+                  <a href="product_details.html?lid=${lid}">
+                    <img src="${md}" title="${title}" alt="${title}"></a>
+                </div>  
+                <div class="goods-info">
+                  <div class="goods-pic-scroll-show">
+                    <ul>
+                      <li><a href="#">
+                        <img src="./img/product/sm/dell_1_3_05941227187515089_60.jpg" alt="">
+                      </a></li>
+                      <li><a href="#">
+                        <img src="./img/product/sm/dell_1_3_05941227187515089_60.jpg" alt="">
+                      </a></li>
+                      <li><a href="#">
+                        <img src="./img/product/sm/dell_1_3_05941227187515089_60.jpg" alt="">
+                      </a></li>
+                      <li><a href="#">
+                        <img src="./img/product/sm/dell_1_3_05941227187515089_60.jpg" alt="">
+                      </a></li>
+                      <li><a href="#">
+                        <img src="./img/product/sm/dell_1_3_05941227187515089_60.jpg" alt="">
+                      </a></li>
+                    </ul>
+                  </div>
+                  <div class="goods-title">
+                    <a href="product_details.html?lid=${lid}">${title}</a>
+                  </div>
+                  <div class="goods-price">
+                    <em class="sale-price">￥${price.toFixed(2)}</em>
+                  </div>
+                  <div class="store">
+                    <a href="#">官方自营</a>
+                  </div>
+                  <div class="goods-sub">
+                    <span><i></i>加入对比</span>
+                  </div>
+                  <div class="goods-add" >
+                    <a href="#">
+                      <i class="icon-shopping-cart" data-lid="${lid}"></i>
+                      加入购物车
+                    </a>
+                  </div>
+                  <div class="sell-stat" >
+                    <ul>
+                      <li><a href="#">0</a><p>商品销量</p></li>
+                      <li><a href="#">0</a><p>用户评论</p></li>
+                      <li><a href="#">0</a><p>在线客服</p></li>
+                    </ul>
+                  </div>
+                </div>
+                
+              </div>
+            </li>`;
+        }
+        $("#p_list").html(html)
+      }
+    })
+  }
+
+
+
+})
